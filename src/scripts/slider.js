f54386de29fc1f21ef19280af943e31b24459ee6
Andrey Voronin <AVor0n@yandex.ru>
2024-11-30 19:31:45 +0300

    feat: adaptive slider

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,12 +1,14 @@
 const $slider = document.querySelector('.slider__container');
-const $sliderContainer = document.querySelector('.slider__content');
 const $sliderPrevBtn = document.querySelector('.slider__button_prev');
 const $sliderNextBtn = document.querySelector('.slider__button_next');
 
-$slider.style.left = 0;
 let currentStep = 0;
-const countSteps = 3;
-let sliderStepOffset;
+let countSteps = getStepsCount();
+let sliderStepOffset = getSliderStepOffset();
+
+function getStepsCount() {
+  return matchMedia('(width <= 768px)').matches ? 6 : 3;
+}
 
 function setSliderStep(step) {
   currentStep = step;
@@ -14,15 +16,13 @@ function setSliderStep(step) {
 }
 
 function getSliderStepOffset() {
-  return ($slider.scrollWidth - $sliderContainer.clientWidth) / countSteps;
+  return ($slider.scrollWidth - $slider.clientWidth) / countSteps;
 }
 
 function updateBtnState(step) {
   $sliderPrevBtn.disabled = step === 0;
   $sliderNextBtn.disabled = step === countSteps;
 }
-
-sliderStepOffset = getSliderStepOffset();
 
 $sliderNextBtn.addEventListener('click', () => {
   const newStep = currentStep + 1;
@@ -34,4 +34,11 @@ $sliderPrevBtn.addEventListener('click', () => {
   const newStep = currentStep - 1;
   setSliderStep(newStep);
   updateBtnState(newStep);
+});
+
+window.addEventListener('resize', () => {
+  countSteps = getStepsCount();
+  sliderStepOffset = getSliderStepOffset();
+  setSliderStep(0);
+  updateBtnState(0);
 });
