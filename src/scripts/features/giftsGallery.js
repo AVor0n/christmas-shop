233bb8a025b4ce14ffb2103d604e6b2fdcaa1b94
Andrey Voronin <AVor0n@yandex.ru>
2024-12-02 19:12:38 +0300

    feat: add category switch

--- src/scripts/features/giftsGallery.js
+++ src/scripts/features/giftsGallery.js
@@ -3,9 +3,27 @@ import { getRandomItems } from '../utils/getRandomItems';
 import { createGiftCard } from './giftCard';
 
 const $cardContainer = document.querySelector('.gifts-gallery__cards');
+const $tabs = document.querySelectorAll('.tabs__tab');
 
 const randomGifts = getRandomItems(gifts, gifts.length);
-randomGifts.forEach(giftData => {
-  const $giftCard = createGiftCard(giftData);
-  $cardContainer.append($giftCard);
+
+const fillContainer = data => {
+  data.forEach(giftData => {
+    const $giftCard = createGiftCard(giftData);
+    $cardContainer.append($giftCard);
+  });
+};
+
+$tabs.forEach($tab => {
+  $tab.addEventListener('click', () => {
+    const { category } = $tab.dataset;
+    $cardContainer.innerHTML = '';
+    const filteredGifts = category === 'all' ? randomGifts : randomGifts.filter(gift => gift.category === category);
+    fillContainer(filteredGifts);
+
+    $tabs.forEach(tab => tab.classList.remove('tabs__tab_active'));
+    $tab.classList.add('tabs__tab_active');
+  });
 });
+
+fillContainer(randomGifts);
