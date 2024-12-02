f4c1e968a79c13d52f123ead2c0847dcaa89b9e3
Andrey Voronin <AVor0n@yandex.ru>
2024-12-02 17:49:41 +0300

    fix: burger close button animation

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -1,13 +1,15 @@
-const $burger = document.querySelector('.nav__burger');
+const $burger = document.querySelector('.burger');
 const $navList = document.querySelector('.nav__list');
 const $navLinks = document.querySelectorAll('.nav__link');
 
 $burger.addEventListener('click', () => {
   $navList.classList.toggle('active');
+  $burger.classList.toggle('active');
 });
 
 $navLinks.forEach($link =>
   $link.addEventListener('click', () => {
     $navList.classList.remove('active');
+    $burger.classList.remove('active');
   }),
 );
