164d0fc9652912042effd78d1ee52f01136aaaac
Andrey Voronin <AVor0n@yandex.ru>
2024-12-03 12:10:33 +0300

    fix: burger behavior

--- src/scripts/features/burger.js
+++ src/scripts/features/burger.js
@@ -5,11 +5,13 @@ const $navLinks = document.querySelectorAll('.nav__link');
 $burger.addEventListener('click', () => {
   $navList.classList.toggle('active');
   $burger.classList.toggle('active');
+  document.body.style.overflow = 'hidden';
 });
 
 $navLinks.forEach($link =>
   $link.addEventListener('click', () => {
     $navList.classList.remove('active');
     $burger.classList.remove('active');
+    document.body.style.overflow = 'initial';
   }),
 );
